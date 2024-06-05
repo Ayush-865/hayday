2e41e041ec8fa64ce7c9d0d314de7c2cbe4ab7fd
Ayush <ayush.up105@gmail.com>
2024-06-05 09:27:10 +0530

    updates

---
+++ app/List.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const List = () => {
+  return (
+    <div className="shadow-lg rounded-md p-5 bg-themeforeground md:w-1/2 lg:w-2/3">
+      <h1>List</h1>
+      {/* Add your list content here */}
+    </div>
+  );
+};
+
+export default List;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import List from "./List";
+import Options from "./Options";
+import NavBar from "@/components/NavBar";
 
 const page = () => {
   return (
     <>
-      <h1>home</h1>
+      <div className="min-h-screen p-3 gap-3 bg-themebackground md:flex">
+        <div className="md:w-1/2 lg:w-1/3">
+          <NavBar />
+          <Options />
+        </div>
+
+        <List />
+      </div>
     </>
   );
 };
